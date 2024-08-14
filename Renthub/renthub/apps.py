78101f70effc4dba2b42cf0d1916fc27b8904eaa
kateeba peter <petrkateeb4@gmail.com>
2024-08-14 23:07:52 +0300

    modified to improve performance and implement new features

--- Renthub/renthub/apps.py
+++ Renthub/renthub/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RenthubConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "renthub"
+
+    def ready(self):
+        import renthub.signals
