cbcf48848180c0124e38ff4034cca28d786833f5
kateeba peter <petrkateeb4@gmail.com>
2024-08-14 23:02:14 +0300

    updated urls to map all my view functions in the views.py

--- Renthub/renthub/urls.py
+++ Renthub/renthub/urls.py
@@ -1,15 +1,27 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import PropertyListView, PropertyDetailView, PropertyCreateView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('about/', views.about, name='about'),
     path('', views.home, name='home'),
-    path('properties/', views.property_list, name='property_list'),
-    path('property/<int:pk>/', views.property_detail, name='property_detail'),
+    path('properties/', PropertyListView.as_view(), name='property_list'),
+    path('property/<int:pk>/', PropertyDetailView.as_view(), name='property_detail'),
+    path('property/delete/<int:pk>/', views.property_delete, name='delete_property'),
     path('inbox/', views.inbox, name='inbox'),
     path('message/<int:receiver_id>/', views.send_message, name='send_message'),
-    path('profile/', views.profile, name='profile'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('accounts/profile/', views.profile, name='profile'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/edit-profile/', views.edit_profile, name='edit_profile'),
+
+    path('property/create/', PropertyCreateView.as_view(), name='property_create'),
+    path('dashboard/add-property/', views.add_property, name='add_property'),
+    path('dashboard/edit-property/<int:pk>/', views.edit_property, name='edit_property'),
+    path('my-properties/', views.my_properties, name='my_properties'),
 ]
