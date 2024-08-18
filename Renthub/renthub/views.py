a85b6735901f52435477a945aaa1d4e4fb3a7509
kateeba peter <petrkateeb4@gmail.com>
2024-08-18 22:29:19 +0300

    updated views file to enhance user experience and url rendering

--- Renthub/renthub/views.py
+++ Renthub/renthub/views.py
@@ -1,7 +1,7 @@
 import io
 
 from PIL import Image
-from django.contrib.auth import login, logout
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ObjectDoesNotExist
@@ -9,7 +9,7 @@ from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 
 from .forms import CustomAuthenticationForm
 from .forms import UserProfileForm, ReviewForm, PropertyForm
@@ -25,6 +25,16 @@ def get_user_profile(user):
         return UserProfile.objects.get(user=user)
     except ObjectDoesNotExist:
         return None
+
+
+# helper function to process image
+def process_image(image):
+    img = Image.open(image)
+    img.thumbnail((800, 600), Image.Resampling.LANCZOS)
+    output = io.BytesIO()
+    img.save(output, format='JPEG', quality=85)
+    output.seek(0)
+    return output
 
 
 # Homepage View
@@ -94,17 +104,12 @@ def signup(request):
 
 
 # Logout View
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')
-    return render(request, 'registration/logout.html')
 
 
 # Dashboard View
 @login_required
 def dashboard(request):
-    profile = get_user_profile(request.user)
+    profile = UserProfile.objects.get(user=request.user)
     properties = Property.objects.filter(owner=profile)
     return render(request, 'dashboard.html', {'profile': profile, 'properties': properties})
 
@@ -121,6 +126,7 @@ def profile(request):
     else:
         form = UserProfileForm(instance=user_profile)
     return render(request, 'profile.html', {'form': form})
+
 
 # Send Message View
 @login_required
@@ -141,13 +147,6 @@ def inbox(request):
     user_profile = get_user_profile(request.user)
     messages = Message.objects.filter(receiver=user_profile).select_related('sender', 'property')
     return render(request, 'inbox.html', {'messages': messages})
-
-# My Properties View
-@login_required
-def my_properties(request):
-    user_profile = get_user_profile(request.user)
-    properties = Property.objects.filter(owner=user_profile)
-    return render(request, 'my_properties.html', {'properties': properties})
 
 
 # Property Create View
@@ -172,15 +171,10 @@ def add_property(request):
         if form.is_valid():
             property_instance = form.save(commit=False)
             property_instance.owner = get_user_profile(request.user)
-            # Resize and optimize image
             image = form.cleaned_data.get('image')
             if image:
-                img = Image.open(image)
-                img.thumbnail((800, 600), Image.Resampling.LANCZOS)
-                output = io.BytesIO()
-                img.save(output, format='JPEG', quality=85)
-                output.seek(0)
-                property_instance.save(image.name, output, save=False, content_type='image/jpeg', charset=None)
+                output = process_image(image)
+                property_instance.image.save(image.name, output, save=False)
 
             property_instance.save()
             return redirect('my_properties')
@@ -189,7 +183,7 @@ def add_property(request):
     return render(request, 'add_property.html', {'form': form})
 
 
-# Property Edit View
+# Edit Property View
 @login_required
 def edit_property(request, pk):
     property_instance = get_object_or_404(Property, pk=pk)
@@ -199,14 +193,9 @@ def edit_property(request, pk):
             property_instance = form.save(commit=False)
             property_instance.owner = get_user_profile(request.user)
 
-            # Resize and optimize image if a new one is provided
             image = form.cleaned_data.get('image')
             if image:
-                img = Image.open(image)
-                img = img.resize((800, 600), Image.Resampling.LANCZOS)
-                output = io.BytesIO()
-                img.save(output, format='JPEG', quality=85)
-                output.seek(0)
+                output = process_image(image)
                 property_instance.image.save(image.name, output, save=False)
 
             property_instance.save()
@@ -214,6 +203,8 @@ def edit_property(request, pk):
     else:
         form = PropertyForm(instance=property_instance)
     return render(request, 'edit_property.html', {'form': form})
+
+
 @login_required
 def my_properties(request):
     user_profile = get_user_profile(request.user)
@@ -222,12 +213,14 @@ def my_properties(request):
 
 
 @login_required
-def property_delete(request, pk):
-    property = get_object_or_404(Property, pk=pk)
-    if property.owner and property.owner.user == request.user:
-        property.delete()
-    return redirect('my_properties')
+class PropertyDeleteView(DeleteView):
+    model = Property
+    success_url = reverse_lazy('my_properties')
+    template_name = 'property_confirm_delete.html'
 
+    def get_queryset(self):
+        owner = get_user_profile(self.request.user)
+        return self.model.objects.filter(owner=owner)
 
 # Edit Profile View
 @login_required
@@ -241,6 +234,25 @@ def edit_profile(request):
     else:
         form = UserProfileForm(instance=profile)
     return render(request, 'edit_profile.html', {'form': form})
+
+
+# Add review
+
+@login_required
+def add_review(request, property_id):
+    property_instance = get_object_or_404(Property, id=property_id)
+    reviewer = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.property = property_instance
+            review.reviewer = reviewer
+            review.save()
+            return redirect('property_detail', pk=property_id)
+    else:
+        form = ReviewForm()
+    return render(request, 'add_review.html', {'form': form, 'property': property_instance})
 
 
 # Login View
